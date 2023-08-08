aa713e5fce0642b729b080c40c76f457243a2e23
Erick Romero <erjhrome@bancolombia.com.co>
2023-08-08 15:52:24 -0500

    init proyect

---
+++ src/main/java/com/example/kinesis/kplkinesislibrary/KplKinesisLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinesis.kplkinesislibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KplKinesisLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KplKinesisLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kinesis/kplkinesislibrary/KplKinesisLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinesis.kplkinesislibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KplKinesisLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
