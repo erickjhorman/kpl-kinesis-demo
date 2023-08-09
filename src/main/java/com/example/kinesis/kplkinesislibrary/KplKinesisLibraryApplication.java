17f3218ee587c4ead2a4d0f7506608fa3aedff41
Erick Romero <erjhrome@bancolombia.com.co>
2023-08-09 17:12:17 -0500

    feat: implement CommandLineRunner interfaz

--- src/main/java/com/example/kinesis/kplkinesislibrary/KplKinesisLibraryApplication.java
+++ src/main/java/com/example/kinesis/kplkinesislibrary/KplKinesisLibraryApplication.java
@@ -1,13 +1,18 @@
 package com.example.kinesis.kplkinesislibrary;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KplKinesisLibraryApplication {
+public class KplKinesisLibraryApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KplKinesisLibraryApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
